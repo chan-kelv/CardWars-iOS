08da3e6aa990a92f33f20e6d15dd870467ea4687
Kelvin Chan <kelvin.chan@resaas.com>
2015-07-29 16:39:55 -0700

    Simple single view with view outlets

--- CardWars/ViewController.swift
+++ CardWars/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgLeftCard: UIImageView!
+    @IBOutlet weak var imgRightCard: UIImageView!
+    @IBOutlet weak var btnDraw: UIButton!
+    @IBOutlet weak var imgBackground: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actnDrawTapped(sender: UIButton) {
+        self.btnDraw.setTitle("Draw!", forState: UIControlState.Normal)
+    }
 
 }
 
